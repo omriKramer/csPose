3528f80a17c0faff3b3c4665d144498b9c2d871b
Omri Kramer <omri.kramer@gmail.com>
2019-09-23 14:08:31 +0300

    debugging

--- coco_utils.py
+++ coco_utils.py
@@ -81,16 +81,16 @@ def plot_image_with_kps(img, keypoints, visible=None, ax: Optional[plt.Axes] = N
     if keypoints.size == 0:
         return
 
-    x, y, v = decode_keypoints(keypoints)
-    c = (np.random.random((1, 3)) * 0.6 + 0.4).tolist()[0]
-    sks = np.array(_coco_helper.cats[1]['skeleton']) - 1
-    for sk in sks:
-        if np.all(v[sk] > 0):
-            ax.plot(x[sk], y[sk], linewidth=3, color=c)
-    ax.plot(x[v > 0], y[v > 0], 'o', markersize=8, markerfacecolor=c, markeredgecolor='k', markeredgewidth=2)
-    ax.plot(x[v > 1], y[v > 1], 'o', markersize=8, markerfacecolor=c, markeredgecolor=c, markeredgewidth=2)
-
-    ax.tick_params(axis=u'both', which=u'both', bottom=False, left=False, labelbottom=False, labelleft=False)
+    # x, y, v = decode_keypoints(keypoints)
+    # c = (np.random.random((1, 3)) * 0.6 + 0.4).tolist()[0]
+    # sks = np.array(_coco_helper.cats[1]['skeleton']) - 1
+    # for sk in sks:
+    #     if np.all(v[sk] > 0):
+    #         ax.plot(x[sk], y[sk], linewidth=3, color=c)
+    # ax.plot(x[v > 0], y[v > 0], 'o', markersize=8, markerfacecolor=c, markeredgecolor='k', markeredgewidth=2)
+    # ax.plot(x[v > 1], y[v > 1], 'o', markersize=8, markerfacecolor=c, markeredgecolor=c, markeredgewidth=2)
+    #
+    # ax.tick_params(axis=u'both', which=u'both', bottom=False, left=False, labelbottom=False, labelleft=False)
 
 
 def plot_kps_comparison(oks, images, output, targets):
