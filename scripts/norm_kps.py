fccf2839e72d846fd96f65a2d538994c09a694e7
Omri Kramer <omri.kramer@gmail.com>
2019-11-23 15:50:44 +0200

    receive data path from the command line

--- scripts/norm_kps.py
+++ scripts/norm_kps.py
@@ -1,10 +1,16 @@
+import argparse
+
 from torchvision import transforms as T
 
 from datasets import CocoSingleKPS
 from utils import dataset_mean_and_std
 
+parser = argparse.ArgumentParser()
+parser.add_argument('data_path', type=str)
+args = parser.parse_args()
+
 transform = T.Compose([T.Resize((256, 256)), T.ToTensor()])
-dataset = CocoSingleKPS.from_data_path('/Volumes/waic/shared/coco', train=True, transform=transform)
+dataset = CocoSingleKPS.from_data_path(args.data_path, train=True, transform=transform)
 
 mean, std = dataset_mean_and_std(dataset)
 
