bb67e3ff5b65aaaa1f2485e38c3956fcc9004e5b
Omri Kramer <omri.kramer@gmail.com>
2019-12-29 15:25:49 +0200

    face keypoints

--- experiments/cs_selected_keypoints/with_td_head.py
+++ experiments/cs_selected_keypoints/with_td_head.py
@@ -12,8 +12,7 @@ IMAGE_SIZE = 128, 128
 data_path, remaining_args = utils.get_data_path()
 engine = eng.Engine.command_line_init(args=remaining_args)
 
-selected_kps = ['left_eye']
-train_filter_kps = ['left_eye', 'right_eye']
+selected_kps = ['nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear']
 
 
 def get_transforms(train):
@@ -30,7 +29,7 @@ def get_transforms(train):
 
 
 coco_train = CocoSingleKPS.from_data_path(data_path, train=True, transforms=get_transforms(True),
-                                          keypoints=train_filter_kps)
+                                          keypoints=selected_kps)
 coco_val = CocoSingleKPS.from_data_path(data_path, train=False, transforms=get_transforms(False),
                                         keypoints=selected_kps)
 
@@ -38,8 +37,8 @@ num_instructions = len(selected_kps)
 model = models.resnet18(td_outplanes=64, num_instructions=num_instructions)
 
 td_head = models.TDHead()
-model = models.SequentialInstructor(model, num_instructions, td_head=td_head)
+model = models.SequentialInstructor(model, num_instructions, td_head=td_head, skip_lateral=True)
 
-evaluator = eval.Evaluator(original_size=IMAGE_SIZE, loss='kl')
+evaluator = eval.Evaluator(original_size=IMAGE_SIZE, loss='ce')
 plot = eval.Visualizer(CocoSingleKPS.MEAN, CocoSingleKPS.STD)
 engine.run(model, coco_train, coco_val, evaluator, plot_fn=plot)
