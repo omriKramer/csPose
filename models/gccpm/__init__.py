810c5a3834392575656e495a223bbf652916d713
Omri Kramer <omri.kramer@gmail.com>
2020-01-30 10:27:03 +0200

    bug fixes

--- models/gccpm/__init__.py
+++ models/gccpm/__init__.py
@@ -5,5 +5,5 @@ from .model import SinglePersonPoseEstimationWithMobileNet
 def gccpm(pretrain_checkpoint=None):
     net = SinglePersonPoseEstimationWithMobileNet()
     if pretrain_checkpoint:
-        load(model, pretrain_checkpoint)
+        load(net, pretrain_checkpoint)
     return net
