00eafe7565fc1a710dfa9c3ad3f4a80fccbcecaf
Omri Kramer <omri.kramer@gmail.com>
2020-03-01 11:16:08 +0200

    control amount of epochs for each stage

--- baseline.py
+++ baseline.py
@@ -15,7 +15,7 @@ class RecurrentLoss:
         return pose.pose_ce_loss(outputs[1], targets)
 
 
-def main(n=1):
+def main(n=1, e64=10, e128=50, e256=60):
     name = f'n={n}'
     print(name)
     root = Path(__file__).resolve().parent.parent / 'LIP'
@@ -28,13 +28,15 @@ def main(n=1):
     save_clbk = partial(SaveModelCallback, every='improvement', monitor=monitor, name=f'baseline-{name}', mode='max')
     learn = cs.cs_learner(db, models.resnet18, instructor, td_c=16, pretrained=False, embedding=None,
                           loss_func=RecurrentLoss(n), callback_fns=[pckh, logger, save_clbk])
-    learn.fit_one_cycle(10, 1e-2)
+    if e64 > 0:
+        learn.fit_one_cycle(e64, 1e-2)
 
-    learn.data = pose.get_data(root, 128, bs=64)
-    learn.fit_one_cycle(40, 5e-3)
+    if e128 > 0:
+        learn.data = pose.get_data(root, 128, bs=64)
+        learn.fit_one_cycle(e128, 5e-3)
 
     learn.data = pose.get_data(root, 256, bs=32)
-    learn.fit_one_cycle(60, 1e-4)
+    learn.fit_one_cycle(e256, 1e-4)
 
 
 if __name__ == '__main__':
@@ -42,5 +44,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--n', default=1, type=int)
+    parser.add_argument('--e64', default=10, type=int)
+    parser.add_argument('--e128', default=40, type=int)
+    parser.add_argument('--e256', default=60, type=int)
     args = parser.parse_args()
     main(**vars(args))
