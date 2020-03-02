cafa74c03adaa8148cbabb4612ba238eed1da5df
Omri Kramer <omri.kramer@gmail.com>
2020-03-02 11:51:06 +0200

    update baseline script

--- baseline.py
+++ baseline.py
@@ -16,15 +16,17 @@ class RecurrentLoss:
         return pose.pose_ce_loss(outputs[1], targets)
 
 
-def main(n=1, e64=10, e128=40, e256=60, resnet=18, lr=1e-4):
-    if resnet == 18:
+def main(args):
+    if args.resnet == 18:
         model = models.resnet18
-    elif resnet == 50:
+    elif args.resnet == 50:
         model = models.resnet50
     else:
         raise ValueError
-    name = f'n={n}_e64={e64}_e128={e128}_e256={e256}_resnet{resnet}'
+
+    name = str(args)
     print(name)
+    n = args.niter
     root = Path(__file__).resolve().parent.parent / 'LIP'
     instructor = cs.RecurrentInstructor(n)
     pckh = partial(pose.Pckh, niter=n)
@@ -34,27 +36,19 @@ def main(n=1, e64=10, e128=40, e256=60, resnet=18, lr=1e-4):
     monitor = f'Total_{n - 1}' if n > 1 else 'Total'
     save_clbk = partial(SaveModelCallback, every='improvement', monitor=monitor, name=f'baseline-{name}', mode='max')
     learn = cs.cs_learner(db, model, instructor, td_c=16, pretrained=False, embedding=None,
-                          loss_func=RecurrentLoss(n), callback_fns=[pckh, logger, save_clbk, DataTime])
-    if e64 > 0:
-        learn.fit_one_cycle(e64, 1e-2)
+                          loss_func=RecurrentLoss(n), callback_fns=[pckh, DataTime, save_clbk, logger])
 
-    if e128 > 0:
-        learn.data = pose.get_data(root, 128, bs=64)
-        learn.fit_one_cycle(e128, 5e-3)
-
-    learn.data = pose.get_data(root, 256, bs=32)
-    learn.fit_one_cycle(e256, lr)
+    learn.data = pose.get_data(root, 256, bs=args.bs)
+    learn.fit_one_cycle(args.eplchs, args.lr)
 
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n', default=1, type=int)
-    parser.add_argument('--e64', default=10, type=int)
-    parser.add_argument('--e128', default=40, type=int)
-    parser.add_argument('--e256', default=60, type=int)
+    parser.add_argument('-n', '--niter', default=1, type=int)
+    parser.add_argument('-e', '--epochs', default=60, type=int)
     parser.add_argument('-r', '--resnet', default=18, type=int)
     parser.add_argument('--lr', default=1e-4, type=float)
-    args = parser.parse_args()
-    main(**vars(args))
+    parser.add_argument('--bs', default=32, type=int)
+    main(parser.parse_args())
