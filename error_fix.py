aac3d0c3a3c57af1459eefcce3e16d2a019ac37a
Omri Kramer <omri.kramer@gmail.com>
2020-03-19 17:08:48 +0200

    bug fix

--- error_fix.py
+++ error_fix.py
@@ -28,9 +28,14 @@ class ErrorDetectionNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.heatmaps_bn = nn.BatchNorm2d(16)
-        self.resnet = nn.Sequential(*list(models.resnet18(pretrained=False, num_classes=16 * 3).children()))
+        self.resnet = list(models.resnet18(pretrained=False, num_classes=16 * 3).children())
         self.resnet = nn.Sequential(*self.resnet[4:])
-        self.resnet[0][0].conv1 = nn.Conv2d(16 + 64, 64, kernel_size=3, padding=1)
+        first_block = self.resnet[0]
+        first_block[0].conv1 = nn.Conv2d(16 + 64, 64, kernel_size=3, padding=1)
+        first_block.downsample = nn.Sequential(
+            cs.conv1x1(16 + 64, 64 * first_block.expansion),
+            nn.BatchNorm2d(64 * first_block.exapnsion),
+        )
 
     def forward(self, img_features, td_out):
         out = self.heatmaps_bn(td_out)
