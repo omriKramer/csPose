440c94a86562fe1ee27dbb30ffe0ca26e72821a1
Omri Kramer <omri.kramer@gmail.com>
2020-05-17 11:03:50 +0300

    bug fix

--- broden_scripts/td_per_obj.py
+++ broden_scripts/td_per_obj.py
@@ -9,7 +9,7 @@ def main(args):
     tree = parts.ObjectTree.from_meta_folder(broden_root / 'meta')
     learn = parts.part_learner(db, models.resnet34, tree, pretrained=args.pretrained, sample_one=args.sample_one)
     lr = args.lr
-    if args.lr0:
+    if args.lr0 is not None:
         lr = slice(args.lr0, lr)
     utils.fit_and_log(learn, 'object-P.A.', save=args.save, epochs=args.epochs, start_epoch=args.start_epoch,
                       lr=lr, wd=args.wd, load=args.load, no_one_cycle=args.no_one_cycle)
