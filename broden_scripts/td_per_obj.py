a40fc2b6c9591b8fdbf94fb70449eb896ebea87c
Omri Kramer <omri.kramer@gmail.com>
2020-05-17 10:50:32 +0300

    add option to lr slice

--- broden_scripts/td_per_obj.py
+++ broden_scripts/td_per_obj.py
@@ -8,8 +8,11 @@ def main(args):
     db = parts.get_data(broden_root, bs=args.bs)
     tree = parts.ObjectTree.from_meta_folder(broden_root / 'meta')
     learn = parts.part_learner(db, models.resnet34, tree, pretrained=args.pretrained, sample_one=args.sample_one)
+    lr = args.lr
+    if args.lr0:
+        lr = slice(args.lr0, lr)
     utils.fit_and_log(learn, 'object-P.A.', save=args.save, epochs=args.epochs, start_epoch=args.start_epoch,
-                      lr=args.lr, wd=args.wd, load=args.load, no_one_cycle=args.no_one_cycle)
+                      lr=lr, wd=args.wd, load=args.load, no_one_cycle=args.no_one_cycle)
 
 
 if __name__ == '__main__':
