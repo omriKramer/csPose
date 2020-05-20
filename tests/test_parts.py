dc7a87db5e2bca31c88516d5deaf476c215083bd
Omri Kramer <omri.kramer@gmail.com>
2020-05-20 18:28:16 +0300

    bug fix

--- tests/test_parts.py
+++ tests/test_parts.py
@@ -2,11 +2,24 @@ from fastai.vision import *
 import parts
 
 
+class StubLearn:
+    pass
+
+
 def test_cs():
     tree = parts.ObjectTree.from_meta_folder('./broden_meta')
-    body = create_body(models.resnet34, False)
+    obj_gt = torch.randint(low=0, high=6, size=(8, 256, 256))
+    part_gt = torch.randint(low=-1, high=3, size=(8, 256, 256))
+    learn = StubLearn()
+    metrics = parts.BrodenMetrics(learn, tree, preds_func=tree.cs_preds_func, restrict=False)
+    part_gt[part_gt == 1] = 82
+    part_gt[part_gt == 2] = 5
+    gt = obj_gt, part_gt
+    body = create_body(models.resnet18, False)
     m = parts.CsNet(body, tree)
     img = torch.rand([8, 3, 256, 256])
-    gt = torch.randint(low=0, high=4, size=(8, 256, 256)), []
     out = m(img, gt)
-    assert out is not None
+    crit = parts.Loss(tree)
+    loss = crit(out, obj_gt, part_gt)
+    metrics.on_epoch_begin()
+    metrics.on_batch_end(out, gt, False)
