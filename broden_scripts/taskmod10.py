810074230160058ffd300512a4a933d8a294f6d0
Omri Kramer <omri.kramer@gmail.com>
2020-07-08 11:55:00 +0300

    option to init embeddings with ones

--- broden_scripts/taskmod10.py
+++ broden_scripts/taskmod10.py
@@ -5,6 +5,7 @@ import utils
 from models.taskmod import taskmod
 
 
+
 def main(args):
     broden_root = Path(args.root).resolve()
     tree = parts.ObjectTree.from_meta_folder(broden_root / 'meta')
@@ -12,8 +13,10 @@ def main(args):
     cls10 = ['building', 'person', 'table', 'bicycle', 'chair', 'car', 'door', 'dog', 'cat', 'bird']
     obj10 = [i for i, name in enumerate(tree.obj_names) if name in cls10]
     model, instructor = taskmod(broden_root, tree, obj_classes=obj10, full_head=args.full_head)
-    metrics = partial(parts.BinaryBrodenMetrics, obj_tree=tree, thresh=0.5, obj_classes=obj10)
+    if args.fill_ones:
+        model.embeddings.apply(lambda m: nn.init.ones_(m.weight))
 
+    metrics = partial(parts.BinaryBrodenMetrics, obj_tree=tree, thresh=0.5, obj_classes=obj10)
     learn = Learner(db10, model, loss_func=instructor.loss, callbacks=[instructor], callback_fns=metrics)
     learn.split((learn.model.embeddings,))
     learn.freeze()
@@ -24,4 +27,5 @@ def main(args):
 if __name__ == '__main__':
     parser = utils.basic_broden_parser()
     parser.add_argument('--full-head', action='store_true')
+    parser.add_argument('--fill-ones', action='store_true')
     main(parser.parse_args())
